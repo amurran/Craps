ea47e131d43cd481046bbc05f69e5926d5b327f0
Aisling Murran <murranaisling@gmail.com>
2019-01-23 21:03:42 -0800

    Added more user interface.

--- Craps.java
+++ Craps.java
@@ -19,7 +19,7 @@ public class Craps
   public Craps()
   {
     System.out.println("Craps is a game in which you roll two dice. If the sum of those dice adds up to 7, you win. If they add up to 12, 3 or 2 you loose. If you roll anything else, that number becomes your point. The goal then is to roll until you get your point, or die by rolling a seven.");
-    System.out.println("Do you want to play? Y/n");
+    System.out.println("Do you want to play a game of Craps? Y/n");
     String response = in.nextLine();
     if (response.equals("n") || response.equals("N"))
     {
@@ -30,20 +30,20 @@ public class Craps
     {
         while (round)
         {
-          System.out.println("Rolling two dice");
+          System.out.println("Rolling two dice. Good luck.");
           int[] results = dice.getRolls();
           sum = results[0] + results[1];
           System.out.println("You rolled a " + results[0] + " and a " + results[1]);
 
           if ((sum == 7 && firstRound) || (sum == point))
           {
-            System.out.println("You won!");
+            System.out.println("You won! :)");
             round = false;
           }
 
           else if (((sum == 12 || sum == 3 || sum == 2) && firstRound) || sum == 7)
           {
-            System.out.println("You lost!");
+            System.out.println("You lost! :(");
             round = false;
           }
 
@@ -57,7 +57,7 @@ public class Craps
 
           else
           {
-            System.out.println("Click enter to roll again");
+            System.out.println("Survived another round! Congrats. Click enter to roll again");
             response = in.nextLine();
           }
 
